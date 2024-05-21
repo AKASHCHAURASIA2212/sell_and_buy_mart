1a221040e0b41a0e932f44a23c93898ac3c8d863
AKASH <AKASHCHAURASIA2212@gmail.com>
2024-05-21 22:34:50 +0530

    updates

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,5 +1,5 @@
-let api_url = "http://localhost:3000";
-// let api_url = "https://sell-and-buy-mart-backend.vercel.app";
+// let api_url = "http://localhost:3000";
+let api_url = "https://sell-and-buy-mart-backend.vercel.app";
 
 export default api_url;
 
